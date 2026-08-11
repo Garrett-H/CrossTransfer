5a2a73ca825e71b02ebc44d5c5d341f774d32b82
Him <36011099+Garrett-H@users.noreply.github.com>
2026-08-10 20:11:35 -0500

    Add logging

--- crosstransfer/src/main.rs
+++ crosstransfer/src/main.rs
@@ -1,3 +1,8 @@
+use env_logger::{Builder, Env, Target};
+
 fn main() {
     println!("Hello, world!");
+    let env = Env::default().default_filter_or("debug");
+    Builder::from_env(env).target(Target::Stdout).init();
+    println!("Goodbye, world!");
 }
